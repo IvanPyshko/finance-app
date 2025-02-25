1ca27af403b81cadc8628a32de8898b1440e5164
root <root@DESKTOP-2U0AHGV.>
2025-02-25 18:51:12 +0500

    добавил роуты по comments в swagger

--- backend/transaction_service/main.py
+++ backend/transaction_service/main.py
@@ -6,6 +6,8 @@ app = FastAPI()
 app.include_router(journal_routes.router, prefix="/journal", tags=["journal"])
 app.include_router(acct_routes.router, prefix="/acct", tags=["accounts"])
 app.include_router(category_routes.router, prefix="/category", tags=["categories"])
+app.include_router(comment_routes.router, prefix= "/comment", tags = ["comments"])
+
 
 @app.get("/")
 def root():
