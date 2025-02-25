0c4841da2e3d9c00611e25f534fd45759d4cb89a
root <root@DESKTOP-2U0AHGV.>
2025-02-25 18:27:27 +0500

    убрал чувствительные данные из кода

--- backend/transaction_service/database/db.py
+++ backend/transaction_service/database/db.py
@@ -9,11 +9,11 @@ Base = declarative_base()
 # Загружаем переменные окружения
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER", "finance_user")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "people4people")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "finance")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD",)
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
 DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
